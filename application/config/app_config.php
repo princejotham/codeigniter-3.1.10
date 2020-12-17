569129b731a4efcaaed10bdce6f048d26bf1c84c
Prince <princejomino@gmail.com>
2020-12-17 16:20:19 +0800

    Initial Commit

---
+++ application/config/app_config.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/*
+  |--------------------------------------------------------------------------
+  | Website Name
+  |--------------------------------------------------------------------------
+  |
+  |Name of the Website
+  |
+ */
+
+$config['site_name'] = 'http://localhost/codeigniter-3.1.10';
+
+/*
+  |--------------------------------------------------------------------------
+  | Message key
+  |--------------------------------------------------------------------------
+  |
+  | Message key where message will be kept for future use
+ */
+$config['msg_key'] = 'msg_key';
+
+/* End of file app_config.php */
+/* Location: ./application/config/app_config.php */
